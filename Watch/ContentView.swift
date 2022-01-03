71429594340185dcfdd815414d2b95c48af537c9
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-03 13:19:11 +0100

    Initial Commit

---
+++ Watch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Watch
+//
+//  Created by Rostislav Brož on 1/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Watch/WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchApp.swift
+//  Watch
+//
+//  Created by Rostislav Brož on 1/3/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
