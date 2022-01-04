7ab931b06609d0b6844d669f1da9f50c81836063
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-04 08:25:23 +0100

    Create location request

--- Watch/ViewModels/ContentModel.swift
+++ Watch/ViewModels/ContentModel.swift
@@ -5,16 +5,76 @@
 //  Created by Rostislav Brož on 1/4/22.
 //
 
-import SwiftUI
+import Foundation
+import CoreLocation
 
-struct ContentModel: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class ContentModel: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    var locationManager = CLLocationManager()
+    
+    @Published var authorizationState = CLAuthorizationStatus.notDetermined
+    
+    
+    override init() {
+        
+        // init method of NSObject
+        
+        super.init()
+        
+        
+        // Set content model as the delegate of the location manager
+        
+        locationManager.delegate = self
+        
+        
+        // Request permission from the user
+        
+        locationManager.requestWhenInUseAuthorization()
     }
-}
-
-struct ContentModel_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentModel()
+    
+    // Location manager delegate methods
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        
+        // Update the authorizationState var
+        
+        authorizationState = locationManager.authorizationStatus
+        
+        
+        if locationManager.authorizationStatus == .authorizedAlways || locationManager.authorizationStatus == .authorizedWhenInUse {
+            
+            // Permission granted, all good ->
+            // Start geolocating the user
+            
+            locationManager.startUpdatingLocation()
+        }
+        
+        else if locationManager.authorizationStatus == .denied {
+            
+            // We need permission in order for the app to work as intended
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        // Gives us the location of the user
+        
+        let userLocation = locations.first
+        
+        
+        /* print that prints out the location passed, just for debugging
+         
+        print(locations.first ?? "no location")
+         
+        */
+        
+        
+        if userLocation != nil {
+            
+            // Stop requesting the location after we get it once
+            
+            locationManager.stopUpdatingLocation()
+        }
     }
 }
