41188059e346eed2e24175376cc118e56a956d6b
Rostislav Brož <rostislav.broz@outlook.com>
2022-01-04 08:23:58 +0100

    Create `ContentModel.swift`

---
+++ Watch/ViewModels/ContentModel.swift
@@ -0,0 +1,20 @@
+//
+//  ContentModel.swift
+//  Watch
+//
+//  Created by Rostislav Brož on 1/4/22.
+//
+
+import SwiftUI
+
+struct ContentModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentModel_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentModel()
+    }
+}
